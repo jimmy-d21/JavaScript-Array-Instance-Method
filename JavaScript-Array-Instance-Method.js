ace68a6ea600dd54e40522bacc2c9047efebd25f
jimmyedelacruz <jimmyedelacruz872@gmail.com>
2026-09-18 23:23:30 +0800

    Checking an array literal

---
+++ JavaScript-Array-Instance-Method.js
@@ -0,0 +1,8 @@
+// 1. Array Static Methods
+// Array.isArray()
+// Checks whether the passed value is an Array instance.
+
+// Example 1: Checking an array literal
+console.log(Array.isArray([1, 2, 3]));
+// Output:
+// true
